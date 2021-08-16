21ff5042695438e67ab765a6d436e3a762f9a165
Thu Tran <ngocanhthu20102002@gmail.com>
2021-08-16 19:30:52 +0700

    clean code

--- src/Covid19/resultCalculation.java
+++ src/Covid19/resultCalculation.java
@@ -24,42 +24,14 @@ public class resultCalculation {
 
     //is an accumulated values up to a date
     public static long totalNewVaccinated(ArrayList<CovidData> groupDataArr) {
-        long temp_sum;
-        long sum = 0;
+        long upTo = 0;
         if (groupDataArr.size() > 1) {
-            for (int i = 0; i < groupDataArr.size() - 1; i++) {
-                long vaccinated1 = groupDataArr.get(i).getPeople_vaccinated();
-                long vaccinated2 = groupDataArr.get(i + 1).getPeople_vaccinated();
-                temp_sum = vaccinated2 - vaccinated1;
-                sum += temp_sum;
-            }
+            upTo = groupDataArr.get(groupDataArr.size() - 1).getPeople_vaccinated() - groupDataArr.get(0).getPeople_vaccinated();
         } else {
-            sum += groupDataArr.get(0).getPeople_vaccinated();
+            upTo += groupDataArr.get(0).getPeople_vaccinated();
         }
-        return sum;
-    }
-
-    public static long upToNewCases(ArrayList<CovidData> groupDataArr) {
-        long upTo = 0;
-        // get the last element minus the first element of the array list to get the upto value
-        upTo = groupDataArr.get(groupDataArr.size() - 1).getNew_cases() - groupDataArr.get(0).getNew_cases();
         return upTo;
     }
-
-    public static long upToNewDeaths(ArrayList<CovidData> groupDataArr) {
-        long upTo = 0;
-        // get the last element minus the first element of the array list to get the upto value
-        upTo = groupDataArr.get(groupDataArr.size() - 1).getNew_deaths() - groupDataArr.get(0).getNew_deaths();
-        return upTo;
-    }
-
-    public static long upToNewVaccinated(ArrayList<CovidData> groupDataArr) {
-        long upTo = 0;
-        // get the last element minus the first element of the array list to get the upto value
-        upTo = groupDataArr.get(groupDataArr.size() - 1).getPeople_vaccinated() - groupDataArr.get(0).getPeople_vaccinated();
-        return upTo;
-    }
-
 
     //II.1 Calculate new metrics: TOTAL new cases/new deaths/new vaccinated people in a group.
     //for both 3 grouping options
